204abb0143b5419f43bd7111c1e45664b9fb1070
songfei <623667783@qq.com>
2020-07-02 14:11:54 +0800

    test only commit

--- code/bones/java-kotlin-scala/jktdemo/src/test/java/com/sf/jkt/k/util/GitTest.java
+++ code/bones/java-kotlin-scala/jktdemo/src/test/java/com/sf/jkt/k/util/GitTest.java
@@ -1,4 +1,12 @@
 package com.sf.jkt.k.util;
 
+import org.junit.Test;
+
 public class GitTest {
+    //testRebase
+    @Test
+    public void testGitRebase(){
+        System.out.println("git rebase");
+
+    }
 }
