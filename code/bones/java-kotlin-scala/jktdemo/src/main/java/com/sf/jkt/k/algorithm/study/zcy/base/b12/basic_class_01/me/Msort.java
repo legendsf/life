cd8cbd2dd04244a418b4c67396e83ca4bd00a38a
gaodi <gaodi@gaodideMacBook-Pro.local>
2020-07-03 20:45:52 +0800

    init

--- code/bones/java-kotlin-scala/jktdemo/src/main/java/com/sf/jkt/k/algorithm/study/zcy/base/b12/basic_class_01/me/Msort.java
+++ code/bones/java-kotlin-scala/jktdemo/src/main/java/com/sf/jkt/k/algorithm/study/zcy/base/b12/basic_class_01/me/Msort.java
@@ -34,9 +34,41 @@ public class Msort {
     }
 
 
+    public static void heapSort(int[] arr){
+        if(arr==null||arr.length<2){
+            return;
+        }
+        for(int i=0;i<arr.length;i++){
+            heapInsert(arr,i);
+        }
+        int size=arr.length;
+        Constants.swap(arr,0,--size);
+        while (size>0){
+            heapify(arr,0,size);
+            Constants.swap(arr,0,--size);
+        }
+    }
 
+    public static void heapInsert(int[] arr,int index){
+        while (arr[index]>arr[(index-1)/2]){
+            Constants.swap(arr,index,(index-1)/2);
+            index=(index-1)/2;
+        }
+    }
 
-
+    public static void heapify(int[] arr,int index,int size){
+        int left=index*2+1;
+        while (left<size){
+            int largest=left+1<size&&arr[left+1]>arr[left]?left+1:left;
+            largest=arr[largest]>arr[index]?largest:index;
+            if(largest==index){
+                break;
+            }
+            Constants.swap(arr,index,largest);
+            index=largest;
+            left=index*2+1;
+        }
+    }
 
 
 
@@ -49,6 +81,6 @@ public class Msort {
         Constants.swap(arr,3,1);
 //        Constants.printArray(arr);
 //        Constants.checkSort(Msort::bubbleSort);
-
+            Constants.checkSort(Msort::heapSort);
         }
 }
